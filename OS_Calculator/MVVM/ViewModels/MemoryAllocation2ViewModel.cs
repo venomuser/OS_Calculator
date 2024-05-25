cfeb6bc098371d9aa92b348822e25081fb59a90e
Sajjad Mohammadifar <sajjadmohammadifar96@gmail.com>
2024-05-25 03:46:24 +0330

    writing some lofical memory codes.

--- OS_Calculator/MVVM/ViewModels/MemoryAllocation2ViewModel.cs
+++ OS_Calculator/MVVM/ViewModels/MemoryAllocation2ViewModel.cs
@@ -8,19 +8,40 @@ namespace OS_Calculator.MVVM.ViewModels
     [AddINotifyPropertyChangedInterface]
     public class MemoryAllocation2ViewModel
     {
+        private bool IsReady;
         public List<Processes> Processes { get; set; }
-        public ICommand GoToResultsCommand { get; }
+        public ICommand GoToMemorySizeCommand { get; }
 
         public MemoryAllocation2ViewModel(List<Processes> processes)
         {
             Processes = processes;
-            GoToResultsCommand = new Command(GoToResults);
+            GoToMemorySizeCommand = new Command(GoToMemory);
         }
 
-        private async void GoToResults()
+        private void GoToMemory()
         {
-            //var resultsViewModel = new ResultsViewModel(Processes);
-            //await Application.Current.MainPage.Navigation.PushAsync(new ResultsPage { BindingContext = resultsViewModel });
+            foreach (var proc in Processes)
+            {
+                foreach(var ls in proc.BlockSizesMB)
+                {
+                    if(ls > 2000 || ls < 0 || ls == null)
+                    {
+                        IsReady = false;
+                        App.Current.MainPage.DisplayAlert("Error", "Block Sizes cannot be greater than 2GB or be 0 or be null!","OK");
+                        break;
+                    }
+                }
+                if(!IsReady)
+                    break;
+
+                IsReady = true;
+            }
+            if (IsReady)
+            {
+                //var resultsViewModel = new ResultsViewModel(Processes);
+                //await Application.Current.MainPage.Navigation.PushAsync(new ResultsPage { BindingContext = resultsViewModel });
+            }
+
         }
     }
 }
